44317269d7bc62ce62570231e73dedbf62476422
root <ssalek14@earlham.edu>
2022-03-16 15:26:14 +0000

    Added a function to determine whether an instruction should be removed, added command to remove from parent in the main loop

--- p2.cpp
+++ p2.cpp
@@ -204,6 +204,27 @@ static bool isLiteralMatch(Instruction &a, Instruction &b){
     return false;
 } 
 
+static bool shouldRemoveTrivialDeadCode(Instruction &x){
+    /* Similar to isTriviallyDeadInstruction
+     *
+     * Check whether instruction has any side effects
+     *
+     * Store / Volatie {load,store}/ Branch / Fence
+     * */
+    if (isa<CallInst>(&x) || x.mayHaveSideEffects() ||
+        x.isTerminator()
+       ){
+        return false;
+    }
+
+    if (x.use_empty()){
+        printf("instruction has no use\n");
+        return true;
+    }
+
+    return false;
+}
+
 static void CommonSubexpressionElimination(Module *M) {
 
     // Start with the simplest module: one basic block
@@ -222,9 +243,21 @@ static void CommonSubexpressionElimination(Module *M) {
                 Instruction& inst = *bbi;
                 if (ignoreForCSE(inst)){
                     continue;
+                } else {
+                    // run common subexpression elimination 
+                    if (isLiteralMatch(inst, inst)){
+                        printf("Literal match, remove the first one!\n");        
+                    }
                 }
-                if (isLiteralMatch(inst, inst)){
-                    printf("Literal match, remove the first one!\n");        
+                // FIXME: depending on the order of optimization, you may need
+                // to exit the loop early
+                // run DCE
+                if (shouldRemoveTrivialDeadCode(inst)){
+                    //set the iterator
+                    ++bbi;
+                    //remove this instruction
+                    inst.eraseFromParent();
+
                 }
 		    }
 	    }
